89c6a9e65c7a411fe399bc886d4621ff71d6d703
Miguel Lobato <149892700+miguellobato96@users.noreply.github.com>
2023-11-28 17:00:07 +0000

    Magic Engineering

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 # # # Cria dicionário correspondente à tabela # # # 
 # Abre ficheiro tabela e cria um array, com um index para cada linha
-with open('tabela_classificacao.txt') as file:
+with open('tabela_classificacao.txt', 'r') as file:
     tab_data = file.readlines()
 
 # Guarda os cabeçalhos, que se encontram na primeira linha
@@ -38,26 +38,37 @@ for line in tab_data[1:]:
 
 # Função que reescreve tabela_classificacao.txt
 def tab_update():
-      with open('tabela_classificacao.txt') as file:
+      with open('tabela_classificacao.txt', 'w') as file:
             # Escreve cabeçalhos
             for title in titles:
                 file.write(f"{title},")
-            file.wirte("\n")
+            file.write("\n")
             
             # Escreve os dados para cada equipa
             for team, data in tab.items():
-                file.write(f"{equipa},{data['jogos']},{data['vitorias']},{data['derrotas']},{data['empates']},{data['pontos']}\n")
+                file.write(f"{team},{data['jogos']},{data['vitorias']},{data['derrotas']},{data['empates']},{data['pontos']}\n")
 
+
+def input_key(team):
+    if team.lower() in ("slb", "benfas", "ben", "benfica", "sport lisboa e benfica"):
+        return "SL_Benfica"
+    elif team.lower() in ("scp", "sporting", "sporting clube de portugal", "spo", "sport"):
+        return "Sporting_CP"
+    elif team.lower() in ("porto", "fc porto", "port", "porto futebol clube", "fcp", "futebol clube do porto"):
+        return "FC_Porto"
+    else:
+        return team
+    
 
 # # # # # # # # #
 # Autenticação  #
 # # # # # # # # #
 
-# Abre ficheiro de credenciais registadas e guarda esse valores num array
+# Abre ficheiro de credenciais registadas e guarda os valores dentro de um array
 with open('credenciais.txt') as file:
-        cred = file.readlines()
+    cred = file.readlines()
 
-# Cria um array para os users e outro array para as passwords
+# Cria um array para os users, e outro array para as passwords
 user_data, pw_data = [], []    
 for line in cred:
     u, p = line.split(",")    
@@ -67,34 +78,60 @@ for line in cred:
 # Numero de tentativas
 tentativas = 3
 
-# Verifica se o nome e a password estão na base de dados, se estiverem garante acesso, se não pede ao user para tentar de novo.
+# Pede o nome e password
 request_credentials = True
 while request_credentials == True:
     user_input, pw_input = input("Username: "), input("Password: ")
 
+    # Se as credenciais estiverem corretas, autentica
     if user_input in user_data and pw_input in pw_data:
         request_credentials = False
 
         team_A = input("Digite o nome de uma equipa: ")
         goals_A = int(input(f"Digite o número de golos marcados pelo {team_A}: "))
+
         team_B = input("Digite o nome da outra equipa: ")
         goals_B = int(input(f"Digite o número de golos marcados pelo {team_B}: "))
 
+        input_key_A = input_key(team_A)
+        input_key_B = input_key(team_B)
+
+        # Vitória A & Derrota B
+        if goals_A > goals_B:
+            tab[input_key_A]['jogos'] += 1
+            tab[input_key_A]['vitorias'] += 1
+            tab[input_key_A]['pontos'] += 3
+
+            tab[input_key_B]['jogos'] += 1
+            tab[input_key_B]['derrotas'] += 1
+        # Vitória B & Derrota A
+        elif goals_B > goals_A:
+            tab[input_key_B]['jogos'] += 1
+            tab[input_key_B]['vitorias'] += 1
+            tab[input_key_B]['pontos'] += 3
+
+            tab[input_key_A]['jogos'] += 1
+            tab[input_key_A]['derrotas'] += 1
+        # Empate para ambos
+        else: 
+            tab[input_key_A]['jogos'] += 1
+            tab[input_key_A]['empates'] += 1
+            tab[input_key_A]['pontos'] += 1
+
+            tab[input_key_B]['jogos'] += 1
+            tab[input_key_B]['empates'] += 1
+            tab[input_key_B]['pontos'] += 1
+
+        # Update
+        tab_update()
+            
+    # Decrementa número de tentativas 
     else:
-        print(f"Tente outra vez. \nTem {tentativas} tentativas.")
         tentativas -= 1
-
+        # Quando as tentativas se esgotarem, nega acesso
         if tentativas == 0:
-             request_credentials = False
-             print("Acesso negado.")
-# Se o user falhar 3 vezes gasta as suas tentativas e o seu acesso é negado
-
-        
-# Input de equipa
-def team_input():
-    team_input = input("Insira a equipa: ").lower()
-    return team_input 
-
-# Se a equipa digitada existir na tabela, pede resultado
-if team_input in tab: 
-    input("Insira o resultado: ").lower()
+            request_credentials = False
+            print("Acesso negado.")
+        # Pede login novamente, mostra número de tentativas restantes
+        else: 
+            print(f"Tente outra vez. Tem {tentativas} tentativas.")
