836ddde79d01e17435d3d975e972892f5bd1f703
Miguel Lobato <149892700+miguellobato96@users.noreply.github.com>
2024-01-07 15:59:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,17 @@ Utilização:
 1. Lê e atualiza uma tabela da liga armazenada em 'tabela_classificacao.txt'.
 2. O utilizador é solicitado a fazer autenticação usando credenciais armazenadas em 'credenciais.txt'.
 3. Após uma autenticação bem-sucedida, é apresentado um menu ao utilizador onde poderá escolher entre:
-introduzir novo resultado, consultar tabela e 'zerar' tabela.
+   introduzir novo resultado, consultar tabela e 'zerar' tabela.
 4. A tabela é apresentada utilizando a biblioteca 'matplotlib' e as equipas são ordenadas por pontos e por diferença de golos.
 5. GM -> Golos Marcados | GS -> Golos Sofridos | DG -> Diferença de Golos
 
 Observação:
 - Os nomes das equipas são insensíveis a maiúsculas e minúsculas e podem ser inseridos de diversas formas
- para maior conveniência.
+  para maior conveniência.
 - A aguardar 'updates' futuros.
 """
 
-# Biblioteca para componente gráfica
-from functions import *
+from functions import *  # Import ficheiro functions.py
 
 # Autenticação
 if read_credentials():
