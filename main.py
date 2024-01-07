bb94d899c494ac16d46a470d9e01fc1eb74ed5e4
Miguel Lobato <149892700+miguellobato96@users.noreply.github.com>
2024-01-07 16:00:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,6 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #                              Tabela de Classificação 1ª Divisão Liga Portuguesa                                 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-"""
-Gestão de Tabela de Liga de Futebol
-
-Este programa foi criado para gerir a tabela de classificação da 1.ª Divisão da liga de futebol portuguesa.
-Oferece funcionalidades para atualizar os resultados dos jogos, exibir graficamente a tabela atual e lidar com
-autenticação de utilizador.
-
-Utilização:
-1. Lê e atualiza uma tabela da liga armazenada em 'tabela_classificacao.txt'.
-2. O utilizador é solicitado a fazer autenticação usando credenciais armazenadas em 'credenciais.txt'.
-3. Após uma autenticação bem-sucedida, é apresentado um menu ao utilizador onde poderá escolher entre:
-   introduzir novo resultado, consultar tabela e 'zerar' tabela.
-4. A tabela é apresentada utilizando a biblioteca 'matplotlib' e as equipas são ordenadas por pontos e por diferença de golos.
-5. GM -> Golos Marcados | GS -> Golos Sofridos | DG -> Diferença de Golos
-
-Observação:
-- Os nomes das equipas são insensíveis a maiúsculas e minúsculas e podem ser inseridos de diversas formas
-  para maior conveniência.
-- A aguardar 'updates' futuros.
-"""
 
 from functions import *  # Import ficheiro functions.py
 
