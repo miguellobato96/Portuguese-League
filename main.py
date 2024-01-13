7520b8d1a8c4176a769bf07c033f6cd9b2b40cb8
Miguel Lobato <149892700+miguellobato96@users.noreply.github.com>
2024-01-13 14:33:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,46 +2,46 @@
 #                              Tabela de Classificação 1ª Divisão Liga Portuguesa                                 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-from functions import *  # Import ficheiro functions.py
+from functions import *  # Import functions.py file
 
-# Autenticação
+# Authentication
 if read_credentials():
     authenticated = authenticate_user(read_credentials())
-    # Se as credenciais estiverem corretas, autentica
+    # If the credentials are correct, authenticate
     if authenticated:
         choice = ""
         while choice != "4":
             titles, tab = read_table()
             logo()
             print("--- Menu ---")
-            print("1. Introduzir Novo Resultado")
-            print("2. Consultar Tabela")
-            print("3. Reiniciar Tabela")
-            print("4. Fechar Programa")
-            choice = input("Escolha uma opção (1-4): ")
+            print("1. Enter New Result")
+            print("2. View Table")
+            print("3. Restart Table")
+            print("4. Close Program")
+            choice = input("Choose an option (1-4): ")
 
             if choice == "1":
-                # Input do nome da equipa A
+                # Team A input
                 team_A = ""
                 input_key_A = False
                 while not input_key_A:
-                    team_A = input("Digite o nome de uma equipa: ")
+                    team_A = input("Enter the name of a team: ")
                     input_key_A = input_key(team_A)
 
-                # Input de golos da equipa A
+                # Team A goals input
                 goals_A = get_goals_input(team_A)
 
-                # Input do nome da equipa B
+                # Team B input
                 team_B = ""
                 input_key_B = False
                 while not input_key_B:
-                    team_B = input("Digite o nome da outra equipa: ")
+                    team_B = input("Enter the name of the other team: ")
                     input_key_B = input_key(team_B)
 
-                # Input de golos da equipa B
+                # Team B goals input
                 goals_B = get_goals_input(team_B)
 
-                # Update valores correspondentes a cada equipa
+                # Update values for each team
                 update_scores(input_key_A, goals_A, input_key_B, goals_B)
 
                 # Update
@@ -52,15 +52,15 @@ if read_credentials():
                 display_table(tab)
 
             elif choice == "3":
-                sure = input("Tem a certeza? (s/n)")
-                if sure in ("s", "sim"):
+                sure = input("Are you sure? (y/n)")
+                if sure.lower() in ("y", "yes"):
                     # Reset table
                     tab.clear()
                     reset_table_file()
-                    print("Valores de tabela reiniciados!")
+                    print("Table values reset!")
 
             elif choice == "4":
-                print("Fechando programa... Até já!")
+                print("Closing program... See you soon!")
 
             else:
-                print("Opção inválida!")
+                print("Invalid option!")
